4a9b3ba3f998ec84d7144eaae7c1cf3c0545374d
Your Name <pso130815@gmail.com>
2025-07-31 23:56:23 +0530

    done

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+// C1. Print "Hello, World!" in Java
+
+public class HelloWorld
+{
+    public void printHelloWorld()
+    {
+        System.out.println("Hello, World!");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args)
+    {
+        HelloWorld hw = new HelloWorld();
+        hw.printHelloWorld();
+    }
+}
