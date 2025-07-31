fad80578de41bc75fc4a5ebce845d9644547b728
Your Name <pso130815@gmail.com>
2025-08-01 00:47:48 +0530

    done

---
+++ C3_Sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class C3_Sum 
+{ 
+    public void calculateSum()
+    {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter first number:");
+        int num1=scanner.nextInt();
+        System.out.print("enter second number:");
+        int num2=scanner.nextInt();
+        int sum=num1+num2;
+        System.out.println("The sum is: " + sum);
+    }
+    public static void main(String[] args)
+    {
+        C3_Sum obj=new C3_Sum();
+        obj.calculateSum();
+    }
+
+     
+
+    
+}
